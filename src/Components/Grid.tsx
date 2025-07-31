36f17733d8010e68ef42a3a4696caeafe4bb354c
Tshego <tshegofatsononyane009@gmail.com>
2025-07-31 12:48:31 +0200

    v

--- src/Components/Grid.tsx
+++ src/Components/Grid.tsx
@@ -5,12 +5,16 @@ import img3 from "../Images/Gridimg/promo_apple_watch_series_10_avail_lte__c70y2
 import img5 from "../Images/Gridimg/promo_airpods_pro_2_avail__vkitqi3okwie_large_2x.jpg";
 import img6 from "../Images/Gridimg/promo_apple_card__5cm7draujpey_medium.jpg";
 import img7 from "../Images/Gridimg/hero_logo_apple_watch_series_10__dla4dkv1wfue_largetall_2x.png";
-import img8 from "../Images/Gridimg/promo_iphone_tradein__bugw15ka691e_large.jpg";    
+import img8 from "../Images/Gridimg/promo_iphone_tradein__bugw15ka691e_large.jpg";   
+
+
 const Grid: React.FC = () => {
+  
   return (
+
     <section className="h-[100rem]  mb-5 px-3">
       <div className="grid grid-cols-2 md:grid-cols-2 grid-rows-3 gap-3 h-full">
-        {/* Item 1 */}
+  
         <div className="w-full h-full relative bg-black">
           <img src={img1} alt="iPhone" className="w-full h-full object-cover" />
           <div className="absolute inset-0  flex flex-col justify-center items-center space-y-3 mb-80">
@@ -22,13 +26,10 @@ const Grid: React.FC = () => {
           </div>
         </div>
 
-        {/* Item 2 */}
+
         <div className="w-full h-full">
           <img src={img2} alt="Apple Watch" className="w-full h-full object-cover" />
         </div>
-
-        {/* Item 3 */}
-        
         
         <div className="w-full h-full relative bg-black">
           <img src={img3} alt="iPhone" className="w-full h-full object-cover" />
@@ -41,25 +42,24 @@ const Grid: React.FC = () => {
           </div>
         </div>
 
-        {/* Item 4 */}
+
         <div className="w-full h-full">
           <img src={img5} alt="MacBook Pro" className="w-full h-full object-cover" />
         </div>
 
-        {/* Item 5 */}
+   
         <div className="w-full h-full bg-black">
           <img src={img8} alt="iPhone Trade-In" className="h-full" />
         </div>
 
-        {/* Item 6 */}
-   {/* Item 6 */}
-<div className="w-full h-full flex justify-center items-center bg-gray-100 overflow-hidden">
+           <div className="w-full h-full flex justify-center items-center bg-gray-100 overflow-hidden">
   <img
     src={img6}
     alt="Apple Card"
     className="w-[70%] max-w-7xl h-auto object-contain transform -translate-y-10"
   />
 </div>
+
 
 
       </div>
--- src/Pages/Mac.tsx
+++ src/Pages/Mac.tsx
@@ -8,49 +8,47 @@ import Advert from '../Components/MacComponents/Advert'
 
 
 
-const Mac:React.FC = () => {
+const Mac: React.FC = () => {
   return (
- <section className='h-[500vh] pt-14'>
-    <Icons />
-    <Tagline/>
- <div>
-  <div className='flex justify-between pt-10  px-28'>
-  <h1 className='text-7xl font-semibold'>Mac</h1>
-  <div className='font-semibold text-2xl pt-10'>
-   <h1 className='text-3xl  px-2'>If you can dream it,</h1> 
-    <h1 className='text-3xl  px-2'>Mac can do it.</h1>
-   </div>
-   </div>
-    <div className='pt-20' >
-      <HeroVid />
-    </div>
-    <div className="flex font-semibold text-6xl py-20 ml-24">
-        Get to know Mac
-       </div>
-    </div>
-    <Carousel/>
+    <section className='h-[500vh] pt-14'>
+      <Icons/>
+      <Tagline/>
+      <div>
+        <div className='flex justify-between pt-10  px-28'>
+          <h1 className='text-7xl font-semibold'>Mac</h1>
+          <div className='font-semibold text-2xl pt-10'>
+            <h1 className='text-3xl  px-2'>If you can dream it,</h1>
+            <h1 className='text-3xl  px-2'>Mac can do it.</h1>
+          </div>
+        </div>
+        <div className='pt-20' >
+          <HeroVid/>
+        </div>
+        <div className="flex font-semibold text-6xl py-20 ml-24">
+          Get to know Mac
+        </div>
+      </div>
+      <Carousel/>
+      <div className="flex font-semibold text-6xl py-20 ml-24">
+        Help me choose.
+      </div>
+
+      <div className='w-[90%] mx-auto'>
+        <Advert />
+      </div>
   
-    <div className="flex font-semibold text-6xl py-20 ml-24">
-     Help me choose.
-       </div>
- <div className='w-[90%] mx-auto'>
-   <Advert/>    
-   </div> 
+      <div className="flex justify-between items-center px-24 py-20">
+        <div className="font-semibold text-6xl">
+          Explore the  lineup
+        </div>
 
-   <div className="flex justify-between items-center px-24 py-20">
-  {/* Left Side - Heading */}
-  <div className="font-semibold text-6xl">
-    Explore the  lineup
-  </div>
+        <div className="flex  gap-5 text-right">
+          <a className="text-blue-500 hover:underline  text-lg" href="">Help me choose &gt;</a>
+          <a className="text-blue-500 hover:underline text-lg " href="">Compare all models &gt;</a>
+        </div>
+      </div>
+    </section>
 
-  {/* Right Side - Links */}
-  <div className="flex  gap-5 text-right">
-  <a className="text-blue-500 hover:underline  text-lg" href="">Help me choose &gt;</a>
-  <a className="text-blue-500 hover:underline text-lg " href="">Compare all models &gt;</a>
-  </div>
- </div>
-</section>
   )
 }
-
 export default Mac
